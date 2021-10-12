35729b8ceceed0ba63a6dcd76224ba06f0eafd08
ArmanPoghosyan2001 <a.poghosyan3001@gmail.com>
2021-10-12 21:30:51 +0400

    Add project files.

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    static class Extensions
+    {
+        public static bool TryGetWordFromThird(this string source, out string response)
+        {
+            response = null;
+            bool hasValue = false;
+            for (int i = 2; i < source.Length; i += 3)
+            {
+                response += source[i];
+                hasValue = true;
+            }
+            return hasValue;
+        }
+
+  
+        public static string FirstAndLast(this string source)
+        {
+            return (source[0] + source[source.Length - 1]).ToString();
+        }
+        public static bool GetSumOfOddElements(this IEnumerable<int> source,out int response)
+        {
+            bool hasValue = false;
+            response = 0;
+            foreach (int item in source)
+            {
+                if (item%2==1)
+                {
+                    response += item;
+                    hasValue = true;
+                }
+            }
+            if (!hasValue)
+            {
+                response = default(int);
+            }
+            return hasValue;
+        }
+        public static bool _IsNullOrEmpty<T>(this List<T> source)
+        {
+            return !source.Any();
+        }
+        public static int GetMiddleElement(this List<int> source)
+        {
+            return source[source.Count / 2];
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(556);
+            list.Add(44);
+            bool t = list._IsNullOrEmpty();
+            string k = "aseawk";
+            k.TryGetWordFromThird(out string b);
+
+            Console.WriteLine(list.GetMiddleElement());
+
+
+
+            
+        }
+    }
+}
